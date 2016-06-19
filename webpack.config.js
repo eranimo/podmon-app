994391fe706e4e9f9cbf46044dc700dcd3986b5f
Kaelan Cooter <dcgamers@gmail.com>
2016-06-19 17:20:06 -0400

    production build finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,12 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
-var loaders = [
-  {
-    "test": /\.js?$/,
-    "exclude": /node_modules/,
-    "loader": "babel",
-    "query": {
-      "presets": [
-        "es2015",
-        "react",
-        "stage-0"
-      ],
-      "plugins": []
-    }
-  },
-  {
-    "test": /\.css?$/,
-    "loader": "style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]"
-  },
-  {
-    "test": /\.scss?$/,
-    "loader": "style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass"
-  },
-  {
-    "test": /\.html?$/,
-    "loader": "raw"
-  },
-  {
-    "test": /\.json?$/,
-    "loader": "json"
-  },
-  {
-    "test": /\.png$/,
-    "loader": "url-loader",
-    "query": { "mimetype": "image/png" }
-  }
-];
+var TARGET = process.env.npm_lifecycle_event;
 
-module.exports = {
+var common = {
   devtool: 'eval-source-map',
   entry: path.resolve('src', 'main.js'),
   output: {
@@ -55,7 +22,34 @@ module.exports = {
     })
   ],
   module: {
-    loaders: loaders
+    loaders: [
+      {
+        "test": /\.js?$/,
+        "exclude": /node_modules/,
+        "loader": "babel",
+        "query": {
+          "presets": [
+            "es2015",
+            "react",
+            "stage-0"
+          ],
+          "plugins": []
+        }
+      },
+      {
+        "test": /\.html?$/,
+        "loader": "raw"
+      },
+      {
+        "test": /\.json?$/,
+        "loader": "json"
+      },
+      {
+        "test": /\.png$/,
+        "loader": "url-loader",
+        "query": { "mimetype": "image/png" }
+      }
+    ]
   },
   devServer: {
     historyApiFallback: {
@@ -71,3 +65,53 @@ module.exports = {
     }
   }
 };
+
+if (TARGET === 'start') {
+  module.exports = merge(common, {
+    module: {
+      loaders: [
+        {
+          test: /\.css?$/,
+          loader: "style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]"
+        },
+        {
+          test: /\.scss?$/,
+          loader: "style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass"
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('development')
+        }
+      })
+    ]
+  });
+} else if (TARGET === 'build'){ // production
+  module.exports = merge(common, {
+    devtool: 'cheap-module-source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules")
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules!sass-loader")
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new ExtractTextPlugin("styles.css"),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ]
+  });
+}
